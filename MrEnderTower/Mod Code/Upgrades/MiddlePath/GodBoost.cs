70eea7f332aaf8fc70d7ff027d64d59d9eea4d52
Mani_Dev <56951627+Mani-cwaf@users.noreply.github.com>
2022-07-05 02:11:03 +0530

    Add files via upload

---
+++ MrEnderTower/Mod Code/Upgrades/MiddlePath/GodBoost.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.Towers.Behaviors;
+using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+
+namespace MrEnderTower.Upgrades.MiddlePath
+{
+    public class GodBoost : ModUpgrade<MrEnderTower>
+    {
+        public override int Path => MIDDLE;
+        public override int Tier => 3;
+        public override int Cost => 375000;
+
+        public override string Description => "Increases stats by a large amount";
+
+        public override void ApplyUpgrade(TowerModel tower)
+        {
+            tower.GetBehavior<RateSupportModel>().multiplier *= 0.55f;
+            tower.GetBehavior<DamageSupportModel>().increase += 5;
+            tower.GetBehavior<PierceSupportModel>().pierce += 10;
+            tower.GetBehavior<RangeSupportModel>().additive += 10;
+        }
+    }
+}
---
+++ MrEnderTower/Mod Code/Upgrades/MiddlePath/TowerBoost.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.Towers.Behaviors;
+using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+
+namespace MrEnderTower.Upgrades.MiddlePath
+{
+    public class TowerBoost : ModUpgrade<MrEnderTower>
+    {
+        public override int Path => MIDDLE;
+        public override int Tier => 2;
+        public override int Cost => 195000;
+
+        public override string Description => "Further increases attack speed, and bolts fly even faster";
+
+        public override void ApplyUpgrade(TowerModel tower)
+        {
+            tower.AddBehavior(new RangeSupportModel("GodBoostingRange", true, 0, 5, "RangeMutator", null, false, "EnderGodBoost", "BuffIconTempleSunGod4xx"));
+            tower.AddBehavior(new RateSupportModel("GodBoostingAttackSpeed", .333333f, true, "AttackSpeedMutator", false, 0, null, "EnderGodBoost", "BuffIconTempleSunGod4xx"));
+            tower.AddBehavior(new DamageSupportModel("GodBoostingDamage", true, 4, "DamageMutator", null, false, false, 0.0f));
+            tower.AddBehavior(new PierceSupportModel("GodBoostingPierce", true, 10, "PierceMutator", null, false, "EnderGodBoost", "BuffIconTempleSunGod4xx"));
+
+        }
+    }
+}
