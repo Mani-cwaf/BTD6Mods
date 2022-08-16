31f21e37068f11e43ce47631273153c684a7642a
Mani_Dev <dhingramsp@Gmail.com>
2022-08-16 23:28:18 +0530

    tried once again to fix stuffs

--- Mod Code/BloonChipper/ModHelperData.cs
+++ Mod Code/BloonChipper/ModHelperData.cs
@@ -2,11 +2,12 @@
 {
     public static class ModHelperData
     {
+        public const string Version = "1.1.0";
         public const string Name = "Bloon Chipper";
         public const string Description = "Added the bloonchipper in all it's glory (kinda, only 2 paths complete)";
         public const string RepoOwner = "Mani-cwaf";
         public const string RepoName = "BTD6Mods";
-        public const string SubPath = "Mod%20Code/BloonChipper";
+        public const string SubPath = "main/Mod%20Code/BloonChipper";
         public const string DllName = "BloonChipper.dll";
     }
 }
